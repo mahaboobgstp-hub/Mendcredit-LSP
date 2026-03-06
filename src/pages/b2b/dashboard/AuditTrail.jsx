f7761f825811b7ab427a99fa0f46ddabdaf0d0e4
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-03-06 15:38:44 +0530

    Create AuditTrail.jsx

---
+++ src/pages/b2b/dashboard/AuditTrail.jsx
@@ -0,0 +1,10 @@
+import B2BLayout from "../../../layouts/B2BLayout";
+import AuditTrailContent from "../components/AuditTrailContent";
+
+export default function AuditTrail() {
+  return (
+    <B2BLayout>
+      <AuditTrailContent />
+    </B2BLayout>
+  );
+}
