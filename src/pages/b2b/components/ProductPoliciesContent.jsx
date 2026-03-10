514e918323a756d7f5864a7edaddbed9c506a2c1
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-03-11 02:02:08 +0530

    Update ProductPoliciesContent.jsx

--- src/pages/b2b/components/ProductPoliciesContent.jsx
+++ src/pages/b2b/components/ProductPoliciesContent.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "../../../styles/b2b.css";
-import { metricLibrary } from "./metricLibrary";
+import { MetricLibrary } from "./MetricLibrary";
 
 const loanProducts = [
 "Personal Loan",
