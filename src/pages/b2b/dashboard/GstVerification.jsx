de181d7a53ac802fec5bac11f4440abdd3255622
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-03-10 23:21:44 +0530

    Update GstVerification.jsx

--- src/pages/b2b/dashboard/GstVerification.jsx
+++ src/pages/b2b/dashboard/GstVerification.jsx
@@ -1,1 +1,10 @@
+import B2BLayout from "../../../layouts/B2BLayout";
+import GstVerificationContent from "../components/GstVerificationContent";
 
+export default function GstVerification() {
+  return (
+    <B2BLayout>
+      <GstVerificationContent />
+    </B2BLayout>
+  );
+}
