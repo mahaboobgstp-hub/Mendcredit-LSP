85cfab2135c355b03059fa003b3466b5ee184bcb
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-03-10 23:39:39 +0530

    Create ProvidentFund.jsx

---
+++ src/pages/b2b/dashboard/ProvidentFund.jsx
@@ -0,0 +1,10 @@
+import B2BLayout from "../../../layouts/B2BLayout";
+import ProvidentFundContent from "../components/ProvidentFundContent";
+
+export default function ProvidentFund() {
+  return (
+    <B2BLayout>
+      <ProvidentFundContent />
+    </B2BLayout>
+  );
+}
