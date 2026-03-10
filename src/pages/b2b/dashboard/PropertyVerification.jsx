ce38b8729029a71d31e620a431ef8a0582f1b505
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-03-10 23:56:03 +0530

    Create PropertyVerification.jsx

---
+++ src/pages/b2b/dashboard/PropertyVerification.jsx
@@ -0,0 +1,10 @@
+import B2BLayout from "../../../layouts/B2BLayout";
+import PropertyVerificationContent from "../components/PropertyVerificationContent";
+
+export default function PropertyVerification() {
+  return (
+    <B2BLayout>
+      <PropertyVerificationContent />
+    </B2BLayout>
+  );
+}
